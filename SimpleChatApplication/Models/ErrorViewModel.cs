2f11690a5eef687d9f8c52cbccd8f1041f2adf98
Mariusz Patek <patek.mariusz@wp.pl>
2018-08-06 18:40:46 +0200

    Add project files.

---
+++ SimpleChatApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleChatApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
